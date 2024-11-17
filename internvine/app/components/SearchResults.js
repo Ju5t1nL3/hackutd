0f7cec6cfc63cb9e235fb0bddcc8146f2a25e760
Isaac Chacko <isaac.chacko05@gmail.com>
2024-11-17 01:43:33 -0600

    edits to dashboard

--- internvine/app/components/SearchResults.js
+++ internvine/app/components/SearchResults.js
@@ -6,9 +6,9 @@ import styles from '../styles/SearchResults.module.css';
 export default function SearchResults({ query, filters }) {
   // In a real application, you would fetch results based on the query and filters
   const mockResults = [
-    { id: 1, title: 'Result 1', image: '/image1.jpg' },
-    { id: 2, title: 'Result 2', image: '/image2.jpg' },
-    { id: 3, title: 'Result 3', image: '/image3.jpg' },
+    { id: 1, title: 'Summer 2025 - CUDA Intern', image: '/nvidia.jpg' },
+    { id: 2, title: 'Spring 2025 - Software Development Intern', image: '/google.jpg' },
+    { id: 3, title: 'Spring 2025 - Structural Engineer Intern', image: '/boeing.jpeg' },
   ];
 
   return (
