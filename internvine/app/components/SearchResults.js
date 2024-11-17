3d10b4398b847cd13e45e56f7f4329b1ef8b2328
Isaac Chacko <isaac.chacko05@gmail.com>
2024-11-17 14:15:33 -0600

    asd

--- internvine/app/components/SearchResults.js
+++ internvine/app/components/SearchResults.js
@@ -7,13 +7,17 @@ export default function SearchResults({ query, filters }) {
   const mockResults = [
     { id: 1, title: 'Summer 2025 - Ignite Internship', image: '/nvidia.jpg', company: 'NVIDIA' },
     { id: 2, title: 'Spring 2025 - STEP Program', image: '/google.jpg', company: 'Google' },
-    { id: 3, title: 'Spring 2025 - Structural Engineering Intern', image: '/boeing.jpeg', company: 'Boeing' },
-    { id: 3, title: 'Spring 2025 - Avionics Engineer', image: '/boeing.jpeg', company: 'Boeing' },
-    { id: 3, title: 'Fall 2025 - Flight Engineering Intern', image: '/boeing.jpeg', company: 'Boeing' },
-    { id: 3, title: 'Fall 2025 - Propulsion Research Intern', image: '/boeing.jpeg', company: 'Boeing' },
-    { id: 3, title: 'Fall 2025 - Software Development Intern', image: '/boeing.jpeg', company: 'Boeing' },
-    { id: 4, title: 'Spring 2025 - Virtual Reality Intern', image: '/apple.jpg', company: 'Apple' },
-    { id: 5, title: 'Fall 2025 - Mechanical Engineering Intern', image: '/lockheed.jpg', company: 'Lockheed' },
+    { id: 3, title: 'Spring 2025 - AI Intern', image: '/google.jpg', company: 'Google' },
+    { id: 4, title: 'Spring 2025 - Front End Development Intern', image: '/google.jpg', company: 'Google' },
+    { id: 5, title: 'Fall 2025 - Back End Development Intern', image: '/google.jpg', company: 'Google' },
+    { id: 6, title: 'Summer 2025 - Quantum Intern', image: '/google.jpg', company: 'Google' },
+    { id: 7, title: 'Spring 2025 - Structural Engineering Intern', image: '/boeing.jpeg', company: 'Boeing' },
+    { id: 8, title: 'Spring 2025 - Avionics Engineer', image: '/boeing.jpeg', company: 'Boeing' },
+    { id: 9, title: 'Fall 2025 - Flight Engineering Intern', image: '/boeing.jpeg', company: 'Boeing' },
+    { id: 10, title: 'Fall 2025 - Propulsion Research Intern', image: '/boeing.jpeg', company: 'Boeing' },
+    { id: 11, title: 'Fall 2025 - Software Development Intern', image: '/boeing.jpeg', company: 'Boeing' },
+    { id: 12, title: 'Spring 2025 - Virtual Reality Intern', image: '/apple.jpg', company: 'Apple' },
+    { id: 13, title: 'Fall 2025 - Mechanical Engineering Intern', image: '/lockheed.jpg', company: 'Lockheed' },
   ];
 
   // Ensure filters have default values to avoid undefined errors
